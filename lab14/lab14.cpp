1b41d5000eab18492cb05b27b10ba308a8c10e1b
Богдан Дєнін <Bohdan.Dienin@cit.khpi.edu.ua>
2021-02-21 15:42:46 +0200

    VS_8

---
+++ lab14/lab14.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <Windows.h>
+
+int main()
+{
+
+    WIN32_FIND_DATAW wfd;
+    unsigned long fsize = 0;
+
+    HANDLE const hFind = FindFirstFileW(L"D:\\Study\\Programming\\my_labs\\lab14\\*", &wfd);
+    setlocale(LC_ALL, "");
+
+    if (INVALID_HANDLE_VALUE != hFind)
+    {
+        do
+        {
+            fsize = wfd.nFileSizeHigh;
+            fsize <<= sizeof(wfd.nFileSizeHigh) * 8;
+            fsize |= wfd.nFileSizeLow;
+
+            printf("%ls\t(%.2f KB)\n", &wfd.cFileName[0], (float)fsize / 1000);
+        } while (NULL != FindNextFileW(hFind, &wfd));
+        FindClose(hFind);
+    }
+    return 0;
+}
