e38e4312fba991d045a004dec626ee7c819fb3c0
Богдан Дєнін <Bohdan.Dienin@cit.khpi.edu.ua>
2021-02-25 16:19:20 +0200

    vs_10

---
+++ Semester_01/src/lab15/lab15.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct BOOK
+{
+    char name[100];
+    char сlass[70];
+    int age;
+    char order[70];
+};
+
+bool Binary(const char* filename)
+{
+    BOOK B[4] = {
+      { "Anaconda", "Reptilea", 6, "Squamata" },
+      { "Leopard", "Mammalia", 10, "Carnivora" },
+      { "Hippopotamus", "Mammalia", 36, "Artiodactyla" },
+      { "Giraffe", "Mammalia", 15, "Artiodactyla" }
+    };
+    int n1 = 4;
+    BOOK C[4];
+    int n2;
+    int i;
+
+    ofstream outF(filename, ios::out | ios::binary);
+    if (!outF) return false;
+
+    outF.write((char*)&n1, sizeof(int));
+
+    for (i = 0; i < n1; i++)
+        outF.write((char*)&(B[i]), sizeof(BOOK));
+    cout << "Zoo\n" << endl;
+
+    outF.close();
+
+    ifstream inF(filename, ios::in | ios::binary);
+
+    if (!inF) return false;
+
+    cout << "List of animals:\n";
+
+    inF.read((char*)&n2, sizeof(int));
+
+    for (i = 0; i < n2; i++)
+        inF.read((char*)&(C[i]), sizeof(BOOK));
+
+    inF.close();
+
+    for (i = 0; i < n2; i++)
+    {
+        cout << "Name : " << C[i].name << ", \t";
+        cout << "Class : " << C[i].сlass << ", \t";
+        cout << "Age : " << C[i].age << ", \t";
+        cout << "Order : " << C[i].order << endl;
+    }
+}
+
+void main()
+{
+    Binary("file.bin");
+}
