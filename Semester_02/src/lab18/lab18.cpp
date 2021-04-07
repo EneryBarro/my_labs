57c6da7bd55898ba453cd114efe86bfc0da40c31
Богдан Дєнін <Bohdan.Dienin@cit.khpi.edu.ua>
2021-04-07 13:06:24 +0300

    vs_13

--- Semester_02/src/lab18/lab18.cpp
+++ Semester_02/src/lab18/lab18.cpp
@@ -1,122 +1,106 @@
 ﻿
+
 #include <iostream>
-#include <string>
+#include <forward_list>
+#include<climits>
+#include <windows.h>
+#include <cstring>
+#include <iomanip>
+
+
 
 using namespace std;
 
-typedef string T;
-
-T* add_entry(T* list, const T& new_entry, //
-    int& size, int& capacity);
-T* remove_entry(T* list, const T& delete_me,
-    int& size, int& capacity);
-
-T* allocate(int capacity);
-
-void copy_list(T* dest, T* src, int many_to_copy);
-T* search_entry(T* list, const T& find_me, int size);
-
-void print_list(T* list, int size);
-
-int edit()
+struct Zoo
 {
-	string s("abrakadabra");
+    int number;
+    char name[70];
+    char colour[70];
+    int age;
+};
 
-	const string s2("TEXT2");
-	s.insert(4, s2, 0, 5);
-	cout << s << endl;
+forward_list<Zoo> ZooList;
+Zoo f;
 
-	s.erase(4, 10);
-	cout << s << endl;
 
-	return 0;
-}
-
-int main() 
+/*ФУНКЦИЯ ЗАНОСИТ ДАННЫЕ В НАЧАЛО СПИСКА*/
+void GetData(forward_list<Zoo>& v)
 {
-    edit();
 
-    int capacity = 3;
-    int size = 0;
-    T* list = allocate(capacity);
-    list = add_entry(list, "Name\t\t Class\t\t Age\t Order\n========================================================\n", size, capacity);
-    print_list(list, size);
+    cout << "\n";
+    cout << "Название животного: ";
+    cin >> f.name;
+    cout << "Количество: ";
+    cin >> f.number;
+    cout << "Окрас: ";
+    cin >> f.colour;
+    cout << "Средний возраст: ";
+    cin >> f.age;
 
-    list = add_entry(list, "Anaconda\t Reptilea\t 6\t Squamata\n", size, capacity);
-    print_list(list, size);
-
-
-    list = add_entry(list, "Leopard\t Mammalia\t 10\t Carnivora\n", size, capacity);
-    print_list(list, size);
-
-
-    list = add_entry(list, "Hippopotamus\t Mammalia\t 36\t Artiodactyla\n", size, capacity);
-    print_list(list, size);
-
-
-    list = add_entry(list, "Giraffe\t Mammalia\t 15\t Artiodactyla\n", size, capacity);
-    print_list(list, size);
-
-    cout << "Deleting first string:" << endl;
-
-    list = remove_entry(list, "Name\t\t Class\t\t Age\t Order\n========================================================\n", size, capacity);
-    print_list(list, size);
+    v.push_front(f);
 
 }
 
-T* add_entry(T* list, const T& new_entry,
-    int& size, int& capacity) {
-    if (size == capacity) capacity *= 2;
-    T* new_list = allocate(capacity);
-    copy_list(new_list, list, size);
-    new_list[size] = new_entry;
-    delete[] list;
-    size++;
-    return new_list;
-}
+/*ФУНКЦИЯ ИЗВЛЕКАЕТ ЗАПИСЬ*/
+void ShowData(const forward_list<Zoo>& v)
+{
+    cout << "\n";
+    cout << "Список животных\n";
+    cout << '\n';
 
-T* remove_entry(T* list, const T& delete_me,
-    int& size, int& capacity) {
-    if (size == (int)(capacity / 4)) {
-        capacity = (int)(capacity / 2);
+    cout << setw(20) << "Назвавние животного" << '|';
+    cout << setw(16) << "Количество" << '|';
+    cout << setw(14) << "Окрас" << '|';
+    cout << setw(18) << "Средний возраст" << '|';
+
+    cout << '\n';
+
+    for (const auto& f : v)
+    {
+        cout << setw(20) << f.name << '|';
+        cout << setw(16) << f.number << '|';
+        cout << setw(14) << f.colour << '|';
+        cout << setw(18) << f.age << '|';
+        cout << endl;
     }
-    T* new_list = allocate(capacity);
-    copy_list(new_list, list, size);
-    for (int i = 0, j = 0; j < size; i++) {
-        if (list[i] != *search_entry(list, delete_me, size)) {
-            new_list[j] = list[i];
-            j++;
+    cout << '\n';
+}
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    while (true) {
+
+        int change;
+        cout << "Выберите вариант\n" << endl;
+        cout << "1. Добавить элемент в начало списка\n"
+            << "2. Удалить элемент из начала списка\n"
+            << "3. Вывести элементы списка\n" << endl;
+        cout << ">>> ";
+        cin >> change;
+
+        switch (change) {
+        case 1:
+        {
+            GetData(ZooList);              
+            break;
+        }
+        case 2:
+        {
+            ZooList.pop_front();
+            cout << "Элемент удален..." << endl;
+            break;
+        }
+
+        case 3:
+        {
+            ShowData(ZooList);
+            break;
+        }
+
         }
     }
-    delete[] list;
-    size--;
-    return new_list;
+    return 0;
 }
-
-T* allocate(int capacity) {
-    const bool debug = false;
-    if (debug) cout << "Allocate: capacity: " << capacity << endl;
-    return new T[capacity];
-}
-
-void copy_list(T* dest, T* src, int many_to_copy) {
-    for (int i = 0; i < many_to_copy; i++)
-        dest[i] = src[i];
-}
-
-T* search_entry(T* list, const T& find_me, int size) {
-    for (int i = 0; i < size; i++) {
-        if (list[i] == find_me)
-            return list;
-    }
-    return nullptr;
-}
-
-
-void print_list(T* list, int size) {
-    for (int i = 0; i < size; i++)
-        cout << list[i] << " ";
-    cout << endl;
-}
-
-
